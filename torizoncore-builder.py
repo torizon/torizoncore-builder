9e60597dfaa2410e8ccc9944fafc38e6116b4bcf
Stefan Agner <stefan.agner@toradex.com>
2020-04-03 18:27:59 +0200

    torizoncore-builder: support EULA and release notes documents
    Allow to add EULA and release notes documents to the Toradex Easy
    Installer image.
    
    Related-to: TOR-813
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

--- torizoncore-builder.py
+++ torizoncore-builder.py
@@ -48,7 +48,7 @@ def get_images(feed_url, artifactory_repo, branch, release_type, matrix_build_nu
             yield image
 
 def add_files(tezidir, image_json_filename, filelist, additional_size,
-              image_name, image_description):
+              image_name, image_description, licence_file, release_notes_file):
     image_json_filepath = os.path.join(tezidir, image_json_filename)
     with open(image_json_filepath, "r") as jsonfile:
         jsondata = json.load(jsonfile)
@@ -62,6 +62,12 @@ def add_files(tezidir, image_json_filename, filelist, additional_size,
         jsondata["name"] = image_name
     if image_description is not None:
         jsondata["description"] = image_description
+
+    if licence_file is not None:
+        jsondata["license"] = licence_file
+
+    if release_notes_file is not None:
+        jsondata["releasenotes"] = release_notes_file
 
     jsondata["version"] = jsondata["version"] + ".container"
     jsondata["release_date"] = datetime.datetime.today().strftime("%Y-%m-%d")
@@ -143,16 +149,24 @@ def get_additional_size(output_dir_containers, files_to_add):
     return additional_size
 
 def combine_single_image(source_dir_containers, files_to_add, additional_size,
-                         output_dir, image_name, image_description):
+                         output_dir, image_name, image_description,
+                         licence_file, release_notes_file):
     # Copy container
     for filename in files_to_add:
         filename = filename.split(":")[0]
         shutil.copy(os.path.join(source_dir_containers, filename),
                     os.path.join(output_dir, filename))
 
+    if licence_file is not None:
+        shutil.copy(licence_file, os.path.join(output_dir, licence_file))
+
+    if release_notes_file is not None:
+        shutil.copy(release_notes_file, os.path.join(output_dir, release_notes_file))
+
     for image_file in glob.glob(os.path.join(output_dir, "image*.json")):
         add_files(output_dir, image_file, files_to_add, additional_size,
-                  image_name, image_description)
+                  image_name, image_description, licence_file,
+                  release_notes_file)
 
 def combine_local_image(args):
     output_dir_containers = os.path.abspath(args.bundle_directory)
@@ -179,7 +193,9 @@ def combine_local_image(args):
 
     logging.info("Combining TorizonCore image with Docker Container bundle.")
     combine_single_image(output_dir_containers, DOCKER_FILES_TO_ADD, additional_size,
-                         output_image_dir, args.image_name, args.image_description)
+                         output_image_dir, args.image_name,
+                         args.image_description, args.licence_file,
+                         args.release_notes_file)
     logging.info("Successfully created a TorizonCore image with Docker Containers preprovisioned in {}"
             .format(args.output_directory))
 
@@ -213,8 +229,10 @@ def batch_process(args):
                 logging.info("Downloading from {0}".format(url))
                 downloader.download(url, output_dir)
 
-            combine_single_image(output_dir_containers, DOCKER_FILES_TO_ADD, additional_size,
-                                 output_dir, args.image_name, args.image_description)
+            combine_single_image(output_dir_containers, DOCKER_FILES_TO_ADD,
+                                 additional_size, output_dir, args.image_name,
+                                 args.image_description, args.licence_file,
+                                 args.release_notes_file)
 
             # Start Artifactory upload with a empty environment
             if args.post_script is not None:
@@ -240,7 +258,10 @@ def add_common_image_arguments(subparser):
                         help="""Image name used in Easy Installer image json""")
     subparser.add_argument("--image-description", dest="image_description",
                         help="""Image description used in Easy Installer image json""")
-
+    subparser.add_argument("--image-licence", dest="licence_file",
+                        help="""Licence file which will be shown on image installation""")
+    subparser.add_argument("--image-release-notes", dest="release_notes_file",
+                        help="""Release notes file which will be shown on image installation""")
 
 parser = argparse.ArgumentParser(description="""\
 Utility to create TorizonCore images with containers pre-provisioned. Requires a
