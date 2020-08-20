fe50a8e4f3f39be18a3ce9783c838e22560a99f7
Stefan Agner <stefan.agner@toradex.com>
2020-08-20 13:41:07 +0200

    combine: remove unnecessary exception handling
    Drop local top level Exception handling and rely on the new global
    Exception handling.
    
    Related-to: TOR-1327
    Signed-off-by: Stefan Agner <stefan.agner@toradex.com>

--- tcbuilder/backend/combine.py
+++ tcbuilder/backend/combine.py
@@ -8,23 +8,16 @@ log = logging.getLogger("torizon." + __name__)
 
 def combine_image(image_dir, output_dir_containers, output_directory, image_name,
                   image_description, licence_file, release_notes_file):
-    try:
-        additional_size = common.get_additional_size(output_dir_containers, common.DOCKER_FILES_TO_ADD)
-        if additional_size is None:
-            raise TorizonCoreBuilderError("Docker Container bundle missing, use bundle sub-command.")
+    additional_size = common.get_additional_size(output_dir_containers, common.DOCKER_FILES_TO_ADD)
+    if additional_size is None:
+        raise TorizonCoreBuilderError("Docker Container bundle missing, use bundle sub-command.")
 
-        log.info("Creating copy of TorizonCore source image.")
-        shutil.rmtree(output_directory)
-        shutil.copytree(image_dir, output_directory)
+    log.info("Creating copy of TorizonCore source image.")
+    shutil.rmtree(output_directory)
+    shutil.copytree(image_dir, output_directory)
 
-        log.info("Combining TorizonCore image with Docker Container bundle.")
-        common.combine_single_image(output_dir_containers, common.DOCKER_FILES_TO_ADD, additional_size,
-                                    output_directory, image_name,
-                                    image_description, licence_file,
-                                    release_notes_file)
-    except Exception as ex:
-        if not hasattr(ex, "msg"):
-            ex.msg = "issue occurred while combining image with docker bundle"
-            ex.det = str(ex)
-
-        raise
+    log.info("Combining TorizonCore image with Docker Container bundle.")
+    common.combine_single_image(output_dir_containers, common.DOCKER_FILES_TO_ADD, additional_size,
+                                output_directory, image_name,
+                                image_description, licence_file,
+                                release_notes_file)
