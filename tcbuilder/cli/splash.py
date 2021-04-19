13cdf6cfe1480108a72c13f9fd181cd1778d04fb
Rogerio Guerra Borin <rogerio.borin@toradex.com>
2021-04-19 13:47:44 -0300

    splash: Minor refactoring to simplify reuse
    This is just a minor change to simplify the programmatic use of the
    'splash' command by the high-level 'build' command. This is part of
    the implementations for TOR-1808 (handle customization section of
    the configuration file).
    
    Related-to: TOR-1547
    
    Signed-off-by: Rogerio Guerra Borin <rogerio.borin@toradex.com>

--- tcbuilder/cli/splash.py
+++ tcbuilder/cli/splash.py
@@ -1,32 +1,34 @@
 import os
 import shutil
 import logging
+
 from tcbuilder.errors import PathNotExistError
-from tcbuilder.backend import splash
+from tcbuilder.backend import splash as sbe
 
+log = logging.getLogger("torizon." + __name__)  # use name hierarchy for "main" to be the parent
 
-def splash_subcommand(args):
-    log = logging.getLogger("torizon." + __name__)  # use name hierarchy for "main" to be the parent
+def splash(image, storage_dir, work_dir=None):
+    storage_dir_ = os.path.abspath(storage_dir)
 
-    storage_dir = os.path.abspath(args.storage_directory)
+    work_dir_ = work_dir
+    if work_dir_ is None:
+        work_dir_ = os.path.join(storage_dir_, "splash")
+        if os.path.exists(work_dir_):
+            shutil.rmtree(work_dir_)
+        os.mkdir(work_dir_)
 
-    work_dir = ""
-    if args.work_dir is not None:
-        work_dir = args.work_dir
-    else:
-        work_dir = os.path.join(storage_dir, "splash")
-        if os.path.exists(work_dir):
-            shutil.rmtree(work_dir)
-        os.mkdir(work_dir)
+    image_ = os.path.abspath(image)
+    if not os.path.exists(image_):
+        raise PathNotExistError(f"Unable to find splash image {image_}")
 
-    image = os.path.abspath(args.image)
-    if not os.path.exists(image):
-        raise PathNotExistError(f"Unable to find splash image {image}")
+    src_ostree_archive_dir = os.path.join(storage_dir_, "ostree-archive")
 
-    src_ostree_archive_dir = os.path.join(storage_dir, "ostree-archive")
-
-    splash.create_splash_initramfs(work_dir, image, src_ostree_archive_dir)
+    sbe.create_splash_initramfs(work_dir_, image_, src_ostree_archive_dir)
     log.info("splash screen merged to initramfs")
+
+
+def do_splash(args):
+    splash(args.image, args.storage_directory, work_dir=args.work_dir)
 
 
 def init_parser(subparsers):
@@ -39,4 +41,4 @@ def init_parser(subparsers):
     subparser.add_argument("--work-dir", dest="work_dir",
                            help="""Working directory""")
 
-    subparser.set_defaults(func=splash_subcommand)
+    subparser.set_defaults(func=do_splash)
