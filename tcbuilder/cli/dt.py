bbbe1d61cd86b7d099c961ca3b5c98c5194da9cd
Rogerio Guerra Borin <rogerio.borin@toradex.com>
2021-04-20 10:38:15 -0300

    dt: Minor refactoring to simplify reuse
    This is a simple refactoring to simplify the programmatic use of the 'dt'
    command by the high-level 'build' command. I also took the opportunity
    to clear the pylint warnings (most of them related to long lines). This
    is part of the implementations for TOR-1808 (handle customization section
    of the configuration file).
    
    Related-to: TOR-1547
    
    Signed-off-by: Rogerio Guerra Borin <rogerio.borin@toradex.com>

--- tcbuilder/cli/dt.py
+++ tcbuilder/cli/dt.py
@@ -1,3 +1,7 @@
+"""
+CLI handling for dt subcommand
+"""
+
 import logging
 import os
 import shutil
@@ -18,7 +22,8 @@ def do_dt_status(args):
 
     dtb_basename = dt.get_current_dtb_basename(args.storage_directory)
     if not dtb_basename:
-        log.error("error: cannot identify the enabled device tree in the image because it is dynamically selected at runtime.")
+        log.error("error: cannot identify the enabled device tree in the image "
+                  "because it is dynamically selected at runtime.")
         sys.exit(1)
 
     log.info(f"Current device tree is: {dtb_basename}")
@@ -41,27 +46,30 @@ def do_dt_checkout(args):
         log.debug(traceback.format_exc())  # full traceback to be shown for debugging only
 
 
-def do_dt_apply(args):
-    '''Perform the 'dt apply' command.'''
+def dt_apply(dts_path, storage_dir, include_dirs=None):
+    '''Perform the work of the 'dt apply' command.'''
 
     # Sanity check parameters.
-    assert args.dts_path, "panic: missing device tree source parameter"
-    if not args.include_dirs:
-        args.include_dirs = ["device-trees/include"]
+    assert dts_path, "panic: missing device tree source parameter"
+    if include_dirs is None:
+        include_dirs = ["device-trees/include"]
+
+    log.debug(f"dt_apply: include directories: {','.join(include_dirs)}")
 
     # Compile the device tree.
-    with tempfile.NamedTemporaryFile(delete=False) as f:
-        dtb_tmp_path = f.name
-    if not dt.build_dts(args.dts_path, args.include_dirs, dtb_tmp_path):
-        log.error(f"error: cannot apply {args.dts_path}.")
+    with tempfile.NamedTemporaryFile(delete=False) as file:
+        dtb_tmp_path = file.name
+    if not dt.build_dts(dts_path, include_dirs, dtb_tmp_path):
+        log.error(f"error: cannot apply {dts_path}.")
         sys.exit(1)
 
     # Deploy the device tree blob.
-    dt_changes_dir = dt.get_dt_changes_dir(args.storage_directory)
-    subprocess.check_call(f"rm -rf {dt_changes_dir}", shell=True)  # Erase device tree and overlays of the current session
-    dtb_target_dir = os.path.join(dt_changes_dir, dt.get_dtb_kernel_subdir(args.storage_directory))
+    dt_changes_dir = dt.get_dt_changes_dir(storage_dir)
+    # Erase device tree and overlays of the current session.
+    subprocess.check_call(f"rm -rf {dt_changes_dir}", shell=True)
+    dtb_target_dir = os.path.join(dt_changes_dir, dt.get_dtb_kernel_subdir(storage_dir))
     os.makedirs(dtb_target_dir, exist_ok=True)
-    dtb_target_basename = os.path.splitext(os.path.basename(args.dts_path))[0] + ".dtb"
+    dtb_target_basename = os.path.splitext(os.path.basename(dts_path))[0] + ".dtb"
     dtb_target_path = os.path.join(dtb_target_dir, dtb_target_basename)
     shutil.move(dtb_tmp_path, dtb_target_path)
 
@@ -69,36 +77,58 @@ def do_dt_apply(args):
     uenv_target_dir = os.path.join(dt_changes_dir, "usr", "lib", "ostree-boot")
     os.makedirs(uenv_target_dir, exist_ok=True)
     uenv_target_path = os.path.join(uenv_target_dir, "uEnv.txt")
-    with open(uenv_target_path, "w") as f:
-        f.write(f"fdtfile={dtb_target_basename}\n")
-    subprocess.check_call(f"set -o pipefail && ostree --repo={args.storage_directory}/ostree-archive cat base /usr/lib/ostree-boot/uEnv.txt | sed /^fdtfile=/d >>{uenv_target_path}", shell=True)
+    with open(uenv_target_path, "w") as file:
+        file.write(f"fdtfile={dtb_target_basename}\n")
+    subprocess.check_call(
+        "set -o pipefail && "
+        f"ostree --repo={storage_dir}/ostree-archive "
+        f"cat base /usr/lib/ostree-boot/uEnv.txt | sed /^fdtfile=/d >>{uenv_target_path}",
+        shell=True)
 
     # Deploy an empty overlays config file, so any overlays from the base image are disabled.
     log.info("warning: removing currently applied device tree overlays")
-    with open(os.path.join(dtb_target_dir, "overlays.txt"), "w") as f:
-        f.write("fdt_overlays=\n")
+    with open(os.path.join(dtb_target_dir, "overlays.txt"), "w") as file:
+        file.write("fdt_overlays=\n")
 
     # All set.
     log.info(f"Device tree {dtb_target_basename} successfully applied.")
 
 
+def do_dt_apply(args):
+    '''Perform the 'dt apply' command.'''
+    dt_apply(args.dts_path, args.storage_directory, include_dirs=args.include_dirs)
+
+
 def init_parser(subparsers):
     '''Initializes the 'dt' subcommands command line interface.'''
 
-    parser = subparsers.add_parser("dt", description="Manage device trees", help="Manage device trees")
+    parser = subparsers.add_parser(
+        "dt", description="Manage device trees", help="Manage device trees")
     subparsers = parser.add_subparsers(title='Commands', required=True, dest='cmd')
 
     # dt status
-    subparser = subparsers.add_parser("status", description="Show the currently enabled device tree", help="Show the currently enabled device tree")
+    subparser = subparsers.add_parser(
+        "status",
+        description="Show the currently enabled device tree",
+        help="Show the currently enabled device tree")
     subparser.set_defaults(func=do_dt_status)
 
     # dt checkout
-    subparser = subparsers.add_parser("checkout", description="Checkout Toradex device tree and overlays repository", help="Checkout Toradex device tree and overlays repository")
+    subparser = subparsers.add_parser(
+        "checkout",
+        description="Checkout Toradex device tree and overlays repository",
+        help="Checkout Toradex device tree and overlays repository")
     subparser.set_defaults(func=do_dt_checkout)
 
     # dt apply DEVICE_TREE
-    subparser = subparsers.add_parser("apply", description="Compile and enable a device tree", help="Compile and enable a device tree")
-    subparser.add_argument(metavar="DEVICE_TREE", dest="dts_path", help="Path to the device tree source file")
-    subparser.add_argument("--include-dir", metavar="DIR", dest="include_dirs", action='append', help="Search directory for include files during device tree compilation. Can be passed multiple times. If absent, defaults to 'device-trees/include'.")
+    subparser = subparsers.add_parser(
+        "apply",
+        description="Compile and enable a device tree",
+        help="Compile and enable a device tree")
+    subparser.add_argument(
+        metavar="DEVICE_TREE", dest="dts_path", help="Path to the device tree source file")
+    subparser.add_argument(
+        "--include-dir", metavar="DIR", dest="include_dirs", action='append',
+        help=("Search directory for include files during device tree compilation. "
+              "Can be passed multiple times. If absent, defaults to 'device-trees/include'."))
     subparser.set_defaults(func=do_dt_apply)
-
