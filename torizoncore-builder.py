2189bdf20b05ba7ee5f2d06254a611c0cc98fd23
Jeremias Cordoba <jeremias.cordoba@toradex.com>
2021-04-20 17:44:09 -0700

    bump version to 2.5.4
    Signed-off-by: Jeremias Cordoba <jeremias.cordoba@toradex.com>

--- torizoncore-builder.py
+++ torizoncore-builder.py
@@ -27,7 +27,7 @@ from tcbuilder.errors import TorizonCoreBuilderError
 
 #pylint: enable=wrong-import-position
 
-__version_info__ = ('2', '5', '3')
+__version_info__ = ('2', '5', '4')
 __version__ = '.'.join(__version_info__)
 
 parser = argparse.ArgumentParser(description="""\
