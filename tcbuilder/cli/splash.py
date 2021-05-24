71bc15821deb670180bae29069e589b589bce3e4
Ronaldo Afonso <ronaldo.afonso@toradex.com>
2021-05-24 16:58:30 -0300

    splash: improvements for the CLI parameters
    Remove "--image" and "--work-dir" switches.
    
    Make "image filename" a positional parameter and always use the storage
    volume as the output for the generated "initramfs" file.
    
    Related-to: TOR-1819
    
    Signed-off-by: Ronaldo Afonso <ronaldo.afonso@toradex.com>

--- tcbuilder/cli/splash.py
+++ tcbuilder/cli/splash.py
@@ -1,44 +1,100 @@
+
+"""
+CLI handling for splash subcommand
+"""
+
+import argparse
 import os
 import shutil
 import logging
 
-from tcbuilder.errors import PathNotExistError
+from tcbuilder.errors import PathNotExistError, InvalidArgumentError
 from tcbuilder.backend import splash as sbe
 
 log = logging.getLogger("torizon." + __name__)  # use name hierarchy for "main" to be the parent
 
-def splash(image, storage_dir, work_dir=None):
-    storage_dir_ = os.path.abspath(storage_dir)
 
-    work_dir_ = work_dir
-    if work_dir_ is None:
-        work_dir_ = os.path.join(storage_dir_, "splash")
-        if os.path.exists(work_dir_):
-            shutil.rmtree(work_dir_)
-        os.mkdir(work_dir_)
+def splash(splash_image, storage_dir):
+    """
+    Prepare everything to call the "splash" backend service.
 
-    image_ = os.path.abspath(image)
-    if not os.path.exists(image_):
-        raise PathNotExistError(f"Unable to find splash image {image_}")
+    :param splash_image: Path to the image splash filename.
+    :param storage_dir: Storage volume directory.
+    :raises:
+        PathNotExistError: If could not find the splash image file.
+    """
 
-    src_ostree_archive_dir = os.path.join(storage_dir_, "ostree-archive")
+    storage_dir = os.path.abspath(storage_dir)
 
-    sbe.create_splash_initramfs(work_dir_, image_, src_ostree_archive_dir)
+    work_dir = os.path.join(storage_dir, "splash")
+    if os.path.exists(work_dir):
+        shutil.rmtree(work_dir)
+    os.mkdir(work_dir)
+
+    splash_image = os.path.abspath(splash_image)
+    if not os.path.exists(splash_image):
+        raise PathNotExistError(f"Unable to find splash image {splash_image}")
+
+    src_ostree_archive_dir = os.path.join(storage_dir, "ostree-archive")
+
+    sbe.create_splash_initramfs(work_dir, splash_image, src_ostree_archive_dir)
     log.info("splash screen merged to initramfs")
 
 
 def do_splash(args):
-    splash(args.image, args.storage_directory, work_dir=args.work_dir)
+    """
+    Check for deprecated parameters.
+
+    :param args: Arguments provided to the "isolate" subcommand.
+    :raises:
+        InvalidArgumentError: If a deprecated switch was passed.
+    """
+
+    # Temporary solution to provide better messages (DEPRECATED since 2021-05-17).
+    if args.image_compat:
+        raise InvalidArgumentError(
+            "Error: "
+            "the switch --image has been removed; "
+            "please provide the image filename without passing the switch.")
+
+    # Temporary solution to provide better messages (DEPRECATED since 2021-05-17).
+    if args.work_dir_compat:
+        raise InvalidArgumentError(
+            "Error: "
+            "the switch --work-dir has been removed; "
+            "the initramfs file should be created in storage.")
+
+    splash(args.splash_image, args.storage_directory)
 
 
 def init_parser(subparsers):
-    subparser = subparsers.add_parser("splash",
-                                      help="""change splash screen""")
+    """
+    Parser for "splash" command.
+    """
+    subparser = subparsers.add_parser(
+        "splash",
+        help="change splash screen",
+        epilog="NOTE: the switches --image and --work-dir have been removed.")
 
-    subparser.add_argument("--image", dest="image",
-                           help="""Path and name of splash screen image""",
-                           required=True)
-    subparser.add_argument("--work-dir", dest="work_dir",
-                           help="""Working directory""")
+    subparser.add_argument(
+        dest="splash_image",
+        metavar="SPLASH_IMAGE",
+        help=("Path and name of splash screen image (REQUIRED)."))
+
+    # Temporary solution to provide better messages (DEPRECATED since 2021-05-17).
+    subparser.add_argument(
+        "--image",
+        dest="image_compat",
+        action="store_true",
+        default=False,
+        help=argparse.SUPPRESS)
+
+    # Temporary solution to provide better messages (DEPRECATED since 2021-05-17).
+    subparser.add_argument(
+        "--work-dir",
+        dest="work_dir_compat",
+        type=str,
+        default="",
+        help=argparse.SUPPRESS)
 
     subparser.set_defaults(func=do_splash)
---
+++ tests/integration/testcases/splash.bats
@@ -0,0 +1,42 @@
+
+load 'bats/bats-support/load.bash'
+load 'bats/bats-assert/load.bash'
+load 'bats/bats-file/load.bash'
+
+@test "splash: run without parameters" {
+    run torizoncore-builder splash
+    assert_failure
+    assert_output --partial "the following arguments are required: SPLASH_IMAGE"
+}
+
+@test "splash: check help output" {
+    run torizoncore-builder splash --help
+    assert_success
+    assert_output --partial "Path and name of splash screen image (REQUIRED)."
+}
+
+@test "splash: run with the deprecated --image switch" {
+    run torizoncore-builder splash --image $SAMPLES_DIR/splash/fast-banana.png
+    assert_failure
+    assert_output --partial "Error: the switch --image has been removed"
+    assert_output --partial "please provide the image filename without passing the switch."
+}
+
+@test "splash: run with the deprecated --work-dir switch" {
+    run torizoncore-builder splash --work-dir /tmp \
+                                   $SAMPLES_DIR/splash/fast-banana.png
+    assert_failure
+    assert_output --partial "Error: the switch --work-dir has been removed"
+    assert_output --partial "the initramfs file should be created in storage."
+}
+
+@test "splash: create splash initramfs" {
+    torizoncore-builder images --remove-storage unpack $DEFAULT_TEZI_IMAGE
+
+    run torizoncore-builder splash $SAMPLES_DIR/splash/fast-banana.png
+    assert_success
+    assert_output --partial "splash screen merged to initramfs"
+
+    run torizoncore-builder-shell "ls -l /storage/splash/usr/lib/modules/*/initramfs.img"
+    assert_success
+}
