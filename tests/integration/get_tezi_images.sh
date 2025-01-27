0a79fe62c64568242c2fc72fd7de729e49752a71
Lucas Bernardes <lucas.bernardes@toradex.com>
2025-01-27 15:21:29 -0300

    tests/integration: Refactor get_tezi_images to download latest versioned images
    Previously, get_tezi_images was downloading specific images, that had
    the versions hardcoded on script. This commit changes this to download
    the latest release, monthly or nightly.
    Also, it's possible to define the YOCTO_BRANCH to request a specific
    version of Torizon, e.g. 'kirkstone-6.x.y' and 'scarthgap-7.x.y'.
    Finally, download only the image for the device tested, instead of
    downloading images for all devices.
    
    Related-to: AVAL-18
    
    Signed-off-by: Lucas Bernardes <lucas.bernardes@toradex.com>

--- tests/integration/get_tezi_images.sh
+++ tests/integration/get_tezi_images.sh
@@ -1,94 +1,76 @@
-#!/bin/bash
-
-# images to be downloaded
-# format is MACHINE:DISTRO:IMAGE
-IMAGES="\
-colibri-imx6:torizon-upstream:torizon-docker \
-colibri-imx6ull-emmc:torizon-upstream:torizon-docker \
-colibri-imx7-emmc:torizon-upstream:torizon-docker \
-colibri-imx8x:torizon:torizon-docker \
-apalis-imx6:torizon-upstream:torizon-docker \
-verdin-imx8mm:torizon:torizon-docker \
-verdin-imx8mp:torizon:torizon-docker \
-verdin-am62:torizon:torizon-docker \
-"
+#!/bin/sh
 
 # location to save images
 OUTDIR="$PWD/workdir/images"
-TMPDIR="$OUTDIR/tmp"
 STAMP="$OUTDIR/.images_downloaded"
 
-# branch
-BRANCH="scarthgap-7.x.y"
-
-# CONFIGME: version
-TCVERSION="7.0.0"
-
-# CONFIGME: build number
-BUILD_NUMBER="1"
-BUILD_DATE="20210315"
-
-# CONFIGME: uncomment for nightly images
-#BUILD_RELEASE="prerelease"
-#BUILD_TYPE="nightly"
-#VERSION="${TCVERSION}-devel-${BUILD_DATE}+build.${BUILD_NUMBER}"
-
-# CONFIGME: uncomment for monthly images
-#BUILD_RELEASE="prerelease"
-#BUILD_TYPE="monthly"
-#VERSION="${TCVERSION}-devel-${BUILD_DATE}+build.${BUILD_NUMBER}"
-
-# CONFIGME: uncomment for quarterly images
-BUILD_RELEASE="prod"
-BUILD_TYPE="release"
-VERSION="${TCVERSION}+build.${BUILD_NUMBER}"
-
 prepare() {
-    mkdir -p $OUTDIR
-    rm -Rf $TMPDIR $STAMP
-}
-
-cleanup() {
-    rm -Rf $TMPDIR
-}
-
-download_tezi_image() {
-    OE_MACHINE=$1
-    OE_DISTRO=$2
-    OE_IMAGE=$3
-
-    IMAGE_FILE="${OE_IMAGE}-${OE_MACHINE}-Tezi_${VERSION}${CONTAINER}.tar"
-    LINK="https://artifacts.toradex.com/artifactory/torizoncore-oe-${BUILD_RELEASE}-frankfurt/${BRANCH}/${BUILD_TYPE}/${BUILD_NUMBER}/${OE_MACHINE}/${OE_DISTRO}/${OE_IMAGE}/oedeploy/${IMAGE_FILE}"
-
-    if [ -e $OUTDIR/$IMAGE_FILE ]; then
-        echo "Image file $IMAGE_FILE already downloaded. Skiping."
-    else
-        echo "Downloading $IMAGE_FILE..."
-        if ! wget --no-verbose -P $TMPDIR $LINK; then
-            echo "Error: could not download $IMAGE_FILE."
-            exit 1
-        fi
-        mv $TMPDIR/$IMAGE_FILE $OUTDIR/
-        echo ""
-    fi
+    rm -rf workdir/images
+    mkdir -p "$OUTDIR"
 }
 
 download() {
-    for image in $IMAGES; do
-        MACHINE=$(echo $image | cut -d':' -f 1)
-        DISTRO=$(echo $image | cut -d':' -f 2)
-        IMAGE=$(echo $image | cut -d':' -f 3)
-        download_tezi_image $MACHINE $DISTRO $IMAGE
+    if [ "$TARGET_BUILD_TYPE" = "release" ]; then
+        artifactory_url="https://artifacts.toradex.com/artifactory/torizoncore-oe-prod-frankfurt/$YOCTO_BRANCH/$TARGET_BUILD_TYPE"
+    elif [ "$TARGET_BUILD_TYPE" = "nightly" ]; then
+        artifactory_url="https://artifacts.toradex.com/artifactory/torizoncore-oe-prerelease-frankfurt/$YOCTO_BRANCH/$TARGET_BUILD_TYPE"
+    else
+        echo "ERROR: Invalid TARGET_BUILD_TYPE. Expected 'nightly' or 'release'."
+        exit 1
+    fi
+
+    # Step 1: Catch all build numbers for a specific machine, in descending order
+    build_numbers=$(wget -qO- "$artifactory_url" | grep -o '<a href="[0-9]*/"' |
+                    sed -e 's#<a href="##' -e 's#/"##' | sort -nr)
+
+    # Step 2: Find the highest build number for $TCB_MACHINE
+    for build in $build_numbers; do
+        machine_url="${artifactory_url}/${build}/${TCB_MACHINE}/"
+
+        # Checks if machine exists in current build
+        if wget --spider -q "$machine_url"; then
+            latest_build=$build
+            break  # Exit the loop when it finds the first valid build
+        fi
     done
 
-    touch $STAMP
-    echo "All images successfully downloaded."
+    if [ -z "$latest_build" ]; then
+        echo "ERROR: No image found for $TCB_MACHINE."
+        exit 1
+    fi
+
+    torizon_tar_url="${artifactory_url}/${latest_build}/${TCB_MACHINE}"
+
+    # 'torizon' or 'torizon-upstream'
+    upstream_variant=$(wget -qO- "$torizon_tar_url" | grep '<a href="torizon' |
+                        sed -n 's#.*<a href="\([^"/]*\)/".*#\1#p' |
+                        grep -E '^(torizon|torizon-upstream)$' | head -n1)
+    torizon_tar_url="$torizon_tar_url/$upstream_variant"
+
+    # 'torizon-docker' or 'torizon-core-docker'
+    docker_type=$(wget -qO- "$torizon_tar_url" | grep '<a href="torizon' |
+                    grep 'docker/' | sed -n 's#.*<a href="\([^"/]*\)/".*#\1#p' |
+                    tail -n1)
+    torizon_tar_url="$torizon_tar_url/$docker_type/oedeploy"
+
+    # Find Torizon .tar image
+    filename=$(wget -qO- "$torizon_tar_url" | grep '<a href="' |
+                sed -n 's#.*<a href="\([^"]*-Tezi_[^"]*\.tar\)".*#\1#p' |
+                sort -r | head -n1)
+    torizon_tar_url="$torizon_tar_url/$filename"
+
+    wget --no-verbose -P workdir/images "$torizon_tar_url"
 }
 
 main() {
     prepare
     download
-    cleanup
+    if [ $? -ne 0 ]; then
+        echo "ERROR: Couldn't download Torizon image"
+        exit 1
+    fi
+    touch "$STAMP"
+    echo "Image successfully downloaded."
 }
 
 main
