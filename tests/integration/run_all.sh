f101c3f4839c20f043578ce4ab3653011267acbe
Lucas Bernardes <lucas.bernardes@toradex.com>
2025-04-24 09:02:17 -0300

    run_all: Remove condition to check if get_images scripts exist
    get_tezi_images and get_raw_images are part of the repo, so a condition to
    check if they exist is not necessary.
    
    Signed-off-by: Lucas Bernardes <lucas.bernardes@toradex.com>

--- tests/integration/run_all.sh
+++ tests/integration/run_all.sh
@@ -129,11 +129,7 @@ fi
 if [ "$(echo "$IS_WIC" | tr '[:upper:]' '[:lower:]')" != "true" ]; then
     if [ ! -e workdir/images/.images_downloaded ]; then
         echo "Images not found. Attempting to download images..."
-
-        if [ -f "./get_tezi_images.sh" ]; then
-            echo "Downloading TEZI images..."
-            ./get_tezi_images.sh
-        fi
+        ./get_tezi_images.sh
 
         if [ ! -e workdir/images/.images_downloaded ]; then
             echo "Error: Images not found and could not be downloaded automatically."
@@ -146,11 +142,7 @@ fi
 if [ "$(echo "$IS_WIC" | tr '[:upper:]' '[:lower:]')" = "true" ]; then
     if [ ! -e workdir/images/.raw_images_downloaded ]; then
         echo "Raw Images not found. Attempting to download images..."
-
-        if [ -f "./get_raw_images.sh" ]; then
-            echo "Downloading raw images..."
-            ./get_raw_images.sh
-        fi
+        ./get_raw_images.sh
 
         if [ ! -e workdir/images/.raw_images_downloaded ]; then
             echo "Error: Raw Images not found and could not be downloaded automatically."
