70c31899de9c2716def773c910f4ae24bbbf62d7
Rogerio Guerra Borin <rogerio.borin@toradex.com>
2025-10-01 21:40:49 -0300

    tests/integration: Allow fetching signed images plus improvements
    Add a series of improvements to "get_tezi_images.sh", the image fetching
    script:
    
    - Allow the Artifactory URLs to be specified (overridden) through env
      variables.
    
    - Allow the acceptable distros to be specified, so only those will be
      considered for download (e.g. "torizon torizon-upstream").
    
    - Allow the acceptable variants to be specified, so only those will be
      considered for download (e.g. "torizon-docker torizon-minimal").
    
    - Add support for fetching signed images; at the time of this writing
      those are only available on the internal Artifactory which now can be
      used thanks to the above mentioned improvements.
    
    - Do a deep search rather than shallow search when looking for images.
      Originally the latest build for a machine was found where the images
      where looked for. Now scripts goes over all builds/distros/variants
      untils in finds the specified combination.
    
    Related-to: TCB-756, TCB-740
    
    Signed-off-by: Rogerio Guerra Borin <rogerio.borin@toradex.com>

--- tests/integration/get_tezi_images.sh
+++ tests/integration/get_tezi_images.sh
@@ -1,86 +1,132 @@
-#!/bin/sh
+#!/bin/bash
 
-# location to save images
-OUTDIR="$PWD/workdir/images"
-STAMP="$OUTDIR/.images_downloaded"
-
-# default values
+# default values (commonly overridden)
 TARGET_BUILD_TYPE="${TARGET_BUILD_TYPE:-nightly}"
 YOCTO_BRANCH="${YOCTO_BRANCH:-scarthgap-7.x.y}"
 TCB_MACHINE="${TCB_MACHINE:-colibri-imx6}"
+TARGET_SIGNING_CLASS="${TARGET_SIGNING_CLASS-}"
+
+# low-level control (less commonly overridden)
+ACCEPT_DISTROS=${ACCEPT_DISTROS-torizon torizon-upstream}
+ACCEPT_VARIANTS=${ACCEPT_VARIANTS-torizon-docker torizon-core-docker}
+BASE_ARTIFACTORY_URL_PROD=${BASE_ARTIFACTORY_URL_PROD-https://artifacts.toradex.com/artifactory/torizoncore-oe-prod-frankfurt}
+BASE_ARTIFACTORY_URL_PRERELEASE=${BASE_ARTIFACTORY_URL_PRERELEASE-https://artifacts.toradex.com/artifactory/torizoncore-oe-prerelease-frankfurt}
+
+# logging
+ENABLE_LOG=${ENABLE_LOG:-1}
+ENABLE_DBG=${ENABLE_DBG:-0}
+
+# location to save images
+OUTDIR="${PWD}/workdir/images"
+STAMP="${OUTDIR}/.images_downloaded"
+
+log() {
+    if [ "${ENABLE_LOG}" = "1" ]; then
+        echo "$@"
+    fi
+}
+
+dbg() {
+    if [ "${ENABLE_DBG}" = "1" ]; then
+        echo "$@"
+    fi
+}
+
+error() {
+    echo "ERROR:" "$@" >&2
+    exit 1
+}
 
 prepare() {
     rm -rf workdir/images
-    mkdir -p "$OUTDIR"
+    mkdir -p "${OUTDIR}"
 }
 
 download() {
-    if [ "$TARGET_BUILD_TYPE" = "release" ]; then
-        artifactory_url="https://artifacts.toradex.com/artifactory/torizoncore-oe-prod-frankfurt/$YOCTO_BRANCH/$TARGET_BUILD_TYPE"
-    elif [ "$TARGET_BUILD_TYPE" = "nightly" ]; then
-        artifactory_url="https://artifacts.toradex.com/artifactory/torizoncore-oe-prerelease-frankfurt/$YOCTO_BRANCH/$TARGET_BUILD_TYPE"
+    if [ "${TARGET_BUILD_TYPE}" = "release" ]; then
+        _base_url="${BASE_ARTIFACTORY_URL_PROD:?variable not set}"
+    elif [ "${TARGET_BUILD_TYPE}" = "nightly" ]; then
+        _base_url="${BASE_ARTIFACTORY_URL_PRERELEASE:?variable not set}"
     else
-        echo "ERROR: Invalid TARGET_BUILD_TYPE. Expected 'nightly' or 'release'."
-        exit 1
+        error "Invalid TARGET_BUILD_TYPE. Expected 'nightly' or 'release'."
     fi
 
-    # Step 1: Catch all build numbers for a specific machine, in descending order
-    build_numbers=$(wget -qO- "$artifactory_url" | grep -o '<a href="[0-9]*/"' |
-                    sed -e 's#<a href="##' -e 's#/"##' | sort -nr)
+    artifactory_url="${_base_url}/${YOCTO_BRANCH}/${TARGET_BUILD_TYPE}"
 
-    # Step 2: Find the highest build number for $TCB_MACHINE
-    for build in $build_numbers; do
+    # Determine the available build numbers (limited to 10)
+    build_numbers=$(wget -qO- "${artifactory_url}" | grep -o '<a href="[0-9]*/"' |
+                    sed -e 's#<a href="##' -e 's#/"##' | sort -nr | head -n10)
+
+    torizon_tar_url=""
+
+    # shellcheck disable=SC2086
+    echo "Available build numbers:" ${build_numbers}
+    for build in ${build_numbers}; do
         machine_url="${artifactory_url}/${build}/${TCB_MACHINE}/"
-
-        # Checks if machine exists in current build
-        if wget --spider -q "$machine_url"; then
-            latest_build=$build
-            break  # Exit the loop when it finds the first valid build
+        dbg "Accessing ${machine_url}"
+        if ! wget --spider -q "${machine_url}"; then
+            log "Build #${build}: not available for machine ${TCB_MACHINE}"
+            continue
         fi
+
+        distros=$(wget -qO- "${machine_url}" |
+                      grep -Eo "href=\"(${ACCEPT_DISTROS/ /|})/*\"" |
+                      sed 's/href="\([-a-z]*\).*"/\1/')
+        # shellcheck disable=SC2086
+        log "Build #${build}: available distros:" ${distros}
+
+        for distro in ${distros}; do
+            distro_url="${artifactory_url}/${build}/${TCB_MACHINE}/${distro}/"
+            dbg "Accessing ${distro_url}"
+            variants=$(wget -qO- "${distro_url}" |
+                          grep -Eo "href=\"(${ACCEPT_VARIANTS/ /|})/*\"" |
+                          sed 's/href="\([-a-z]*\).*"/\1/')
+            # shellcheck disable=SC2086
+            log "Build #${build}: available variants:" ${variants}
+
+            for variant in ${variants}; do
+                variant_url="${artifactory_url}/${build}/${TCB_MACHINE}/${distro}/${variant}/"
+
+                # Select between signed classes (if any):
+                if [ -n "${TARGET_SIGNING_CLASS}" ]; then
+                    oedeploy_url="${variant_url}${TARGET_SIGNING_CLASS}/oedeploy/"
+                else
+                    oedeploy_url="${variant_url}oedeploy/"
+                fi
+
+                dbg "Accessing ${oedeploy_url}"
+                filename=$(wget -qO- "${oedeploy_url}" | grep '<a href="' |
+                               sed -n 's#.*<a href="\([^"]*-Tezi_[^"]*\.tar\)".*#\1#p' |
+                               sort -r | head -n1)
+                [ -z "${filename}" ] && continue
+
+                torizon_tar_url="${oedeploy_url}${filename}"
+                # Leave all loops:
+                break 3
+            done
+        done
     done
 
-    if [ -z "$latest_build" ]; then
-        echo "ERROR: No image found for $TCB_MACHINE."
-        exit 1
-    fi
+    [ -n "${torizon_tar_url}" ] || return 1
 
-    torizon_tar_url="${artifactory_url}/${latest_build}/${TCB_MACHINE}"
-
-    # 'torizon' or 'torizon-upstream'
-    upstream_variant=$(wget -qO- "$torizon_tar_url" | grep '<a href="torizon' |
-                        sed -n 's#.*<a href="\([^"/]*\)/".*#\1#p' |
-                        grep -E '^(torizon|torizon-upstream)$' | head -n1)
-    torizon_tar_url="$torizon_tar_url/$upstream_variant"
-
-    # 'torizon-docker' or 'torizon-core-docker'
-    docker_type=$(wget -qO- "$torizon_tar_url" | grep '<a href="torizon' |
-                    grep 'docker/' | sed -n 's#.*<a href="\([^"/]*\)/".*#\1#p' |
-                    tail -n1)
-    torizon_tar_url="$torizon_tar_url/$docker_type/oedeploy"
-
-    # Find Torizon .tar image
-    filename=$(wget -qO- "$torizon_tar_url" | grep '<a href="' |
-                sed -n 's#.*<a href="\([^"]*-Tezi_[^"]*\.tar\)".*#\1#p' |
-                sort -r | head -n1)
-    torizon_tar_url="$torizon_tar_url/$filename"
+    log "Image URL: ${torizon_tar_url}"
 
     # The loading bar of wget is dynamically rendered using terminal control characters,
     # this breaks the logs. So, disable the loading bar in CI
-    if [ "$TCB_UNDER_CI" = "1" ]; then
-        wget --no-verbose -P workdir/images "$torizon_tar_url"
+    if [ "${TCB_UNDER_CI}" = "1" ]; then
+        wget --no-verbose -P workdir/images "${torizon_tar_url}"
     else
-        wget -P workdir/images "$torizon_tar_url"
+        wget -P workdir/images "${torizon_tar_url}"
     fi
 }
 
 main() {
     prepare
-    download
-    if [ $? -ne 0 ]; then
-        echo "ERROR: Couldn't download Torizon image"
+    if ! download; then
+        error "Couldn't download Torizon image"
         exit 1
     fi
-    touch "$STAMP"
+    touch "${STAMP}"
     echo "Image successfully downloaded."
 }
 
